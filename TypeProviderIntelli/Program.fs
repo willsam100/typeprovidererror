61054ef0b8794d984eb7b78c9c5dc355a6259e33
Sam Williams <>
2019-06-08 12:02:01 +1200

    init

---
+++ TypeProviderIntelli/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+open System
+open FSharp.Data
+open System.IO
+
+let [<Literal>] path = "food-price-index-apr19-seasonally-adjusted-csv-tables.csv"
+
+type CSV = CsvProvider<path>
+
+[<EntryPoint>]
+let main argv =
+    printfn "soruce %s " __SOURCE_DIRECTORY__
+
+    let data = File.ReadAllText (__SOURCE_DIRECTORY__ + "/" + path)
+
+    let food = CSV.Parse data
+
+    food.Rows |> Seq.iter (fun x -> printfn "%A" x.)
+    
+    0 // return an integer exit code
